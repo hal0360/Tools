4533ef4e9c96dee5bb0d4cb5a36b12d1851165c6
hal0360 <atromoby@gmail.com>
2018-12-04 16:36:24 +0800

    Added Popup

---
+++ widgets/src/main/java/tw/com/atromoby/widgets/Popup.java
@@ -0,0 +1,73 @@
+package tw.com.atromoby.widgets;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Checkable;
+
+public class Popup implements View.OnClickListener{
+
+    private SparseArray<CmdView> cmds = new SparseArray<>();
+        private Dialog dialog;
+        private Window window;
+
+        public Popup(Context context, int resource_id){
+            dialog = new Dialog(context, R.style.AtromobyDefautDialog);
+            setUp(resource_id);
+        }
+
+        public Popup(Context context, int resource_id, int style_id){
+            dialog = new Dialog(context, style_id);
+            setUp(resource_id);
+        }
+
+        private void setUp(int id){
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(id);
+            window = dialog.getWindow();
+            assert window != null;
+            window.setGravity(Gravity.CENTER);
+        }
+
+        public void clicked(int id, CmdView cd){
+            dialog.findViewById(id).setOnClickListener(this);
+            cmds.put(id,cd);
+        }
+
+        public void clicked(View v, CmdView cd){
+            v.setOnClickListener(this);
+            cmds.put(v.getId(),cd);
+        }
+
+        public void setDimension(double width, double height){
+            window.setLayout((int)width, (int)height);
+        }
+
+        public void setGravity(int gravity){
+            window.setGravity(gravity);
+        }
+
+        public void show(){
+            dialog.show();
+        }
+
+        public <T extends View & Checkable> T findViewById(int id){
+            return dialog.findViewById(id);
+        }
+
+        public void dismiss(){
+            dialog.dismiss();
+        }
+
+        public boolean isShowing(){
+            return dialog.isShowing();
+        }
+
+    @Override
+    public void onClick(View v) {
+        cmds.get(v.getId()).exec(v);
+    }
+}
